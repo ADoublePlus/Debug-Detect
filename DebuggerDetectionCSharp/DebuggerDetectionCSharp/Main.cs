a17558828ddf222f1b44f05f0e13fbbba26ae093
John Wonson <jmpwonson@gmail.com>
2021-02-17 12:45:22 +1100

    Create Main.cs
    Added reference for System.Net.

---
+++ DebuggerDetectionCSharp/DebuggerDetectionCSharp/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Collections.Generic;
+
+namespace debug_detect
+{
+    public class Main
+    {
+        public static void Start(string urlForHashCheck, bool AntiDump, bool AntiDebug, bool AntiEmulation,
+            List<string> AntiNetBlacklist)
+        {
+            WebRequest.DefaultWebProxy = new WebProxy();
+        }
+
+        internal static void CmdWindow(string A_0)
+        {
+            Process.Start(new ProcessStartInfo("cmd.exe", "/c " + A_0)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
+        }
+    }
+}
